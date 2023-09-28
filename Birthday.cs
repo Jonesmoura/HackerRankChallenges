a7c0c3d917833dcbb6e87ad68cfd7a0fc12b5f87
Jones Moura <jones-ms@live.com>
2023-09-28 14:43:23 -0300

    Birthday Challenge

---
+++ Birthday.cs
@@ -0,0 +1,30 @@
+﻿namespace getTotalX
+{
+    public class Birthday
+    {
+        public static int birthday(List<int> s, int d, int m)
+        {
+            int sumLength = m;
+            int sumResult = d;
+            int chocolatesSquares = 0;
+
+            for(int i = 0; i<= (s.Count - m); i++)
+            {
+                int tempSum = 0;
+
+                for(int j = 0; j < sumLength; j++)
+                {
+                    tempSum += s[i + j];
+                }
+
+                if(tempSum == sumResult)
+                {
+                    chocolatesSquares++;
+                }
+            }
+
+            return chocolatesSquares;
+
+        }
+    }
+}
