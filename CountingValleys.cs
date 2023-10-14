e1971758cec72a0aa8d78e8e0eda506d1ae4c9da
Jones Moura <jones-ms@live.com>
2023-10-14 09:30:28 -0300

    CountingValleys and SalesByMath Challenges

---
+++ CountingValleys.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace getTotalX
+{
+    public class CountingValleys
+    {
+        public static int countingValleys(int steps, string path)
+        {
+            int altitude = 1;
+            int valleyCount = 0;
+
+            for(int i = 0; i < steps; i++)
+            {
+                if(altitude == 1 && path[i] == 'D')
+                {
+                    valleyCount++;
+                }
+                if (path[i] == 'D')
+                {
+                    altitude--;
+                }
+                else
+                {
+                    altitude++;
+                }
+
+            }
+
+            return valleyCount;
+        }
+    }
+}
---
+++ SalesByMath.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace getTotalX
+{
+    public class SalesByMath
+    {
+        public static int sockMerchant(int n, List<int> ar)
+        {
+            int pairs = 0;
+            List<int> numbersToCheck = ar;
+
+            for(int j = 0; j < numbersToCheck.Count -1; j++)
+            {
+                for(int i=j+1; i<numbersToCheck.Count; i++)
+                {
+                    if (numbersToCheck[j] == numbersToCheck[i])
+                    {
+                        numbersToCheck.RemoveAt(i);
+                        numbersToCheck.RemoveAt(j);
+                        pairs ++;
+                        j = -1;
+                        break;
+                    }
+                }
+            }
+
+            return pairs;
+
+        }
+    }
+}
