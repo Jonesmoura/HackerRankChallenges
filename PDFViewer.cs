b04c4faf43e088e7498b4e7fe9a4236c7381ec8c
Jones Moura <jones-ms@live.com>
2023-11-30 16:19:01 -0300

    PDFViewer Challenge

---
+++ PDFViewer.cs
@@ -0,0 +1,27 @@
+﻿namespace getTotalX
+{
+    public class PDFViewer
+    {
+        public static int designerPdfViewer(List<int> h, string word)
+        {
+            int totalChars = word.Count();
+            int maxHeight = 0;
+
+            foreach (char letter in word)
+            {
+
+                int index = Convert.ToChar(Char.ToLower(letter)) - 97;
+
+                if (h[index] > maxHeight)
+                {
+
+                    maxHeight = h[index];
+                }
+
+            }
+
+            return maxHeight * totalChars;
+
+        }
+    }
+}
