9644e2effb914a7d9c5ea9595e9049007d99cb97
Jones Moura <jones-ms@live.com>
2023-12-23 10:42:10 -0300

    SaveThePrisoner Challenge

---
+++ SaveThePrisoner.cs
@@ -0,0 +1,20 @@
+﻿namespace getTotalX
+{
+    public class SaveThePrisoner
+    {
+        public static int saveThePrisoner(int n, int m, int s)
+        {
+            int remaining = m % n;
+
+            if((remaining + s -1) % n == 0)
+            {
+                return n;
+            }
+            else
+            {
+                return((remaining + s -1) % n);
+            }
+
+        }
+    }
+}
