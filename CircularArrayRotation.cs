49f9f5ccfbdbc3b0b5d0a00cf560db01e361a3a7
Jones Moura <jones-ms@live.com>
2023-12-27 10:47:23 -0300

    CircularArrayRotation Challenge

---
+++ CircularArrayRotation.cs
@@ -0,0 +1,19 @@
+﻿namespace getTotalX
+{
+    public class CircularArrayRotation
+    {
+        public static List<int> circularArrayRotation(List<int> a, int k, List<int> queries)
+        {
+            int length = a.Count;
+            List<int> result = new List<int>(length);
+            foreach( int i in queries )
+            {
+                int newPos = (length - k + i) % length;
+                result.Add(a[newPos]);
+            }
+
+            return result;
+
+        }
+    }
+}
