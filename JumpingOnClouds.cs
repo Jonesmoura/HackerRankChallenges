c46e9cd85166cf5ead815f285f7365ac0f4540bc
Jones Moura <jones-ms@live.com>
2023-12-29 15:29:48 -0300

    JumpingOnClouds Challenge

---
+++ JumpingOnClouds.cs
@@ -0,0 +1,27 @@
+﻿namespace getTotalX
+{
+    public class JumpingOnClouds
+    {
+        public static int jumpingOnClouds(int[] c, int k)
+        {
+            int energy = 100;
+            int count = 0;
+            int n = c.Length;
+            do
+            {
+                if (c[(count + k) % n] == 0)
+                {
+                    energy--;
+                }
+                else
+                {
+                    energy -= 3;
+                }
+                count += k;
+
+            }while(count % n != 0);
+
+            return energy;
+        }
+    }
+}
