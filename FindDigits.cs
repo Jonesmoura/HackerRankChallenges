9e50bc21dcb7e39e63b051c53abd9f672c803b7b
Jones Moura <jones-ms@live.com>
2024-01-02 11:33:02 -0300

    FindDigits Challenge

---
+++ FindDigits.cs
@@ -0,0 +1,27 @@
+﻿namespace getTotalX
+{
+    public class FindDigits
+    {
+        public static int findDigits(int n)
+        {
+            string nString = n.ToString();
+            int divisors = 0;
+
+            for (int i = 0; i < nString.Length; i++)
+            {
+                int actualNumber = int.Parse(nString[i].ToString());
+                if (actualNumber == 0)
+                {
+                    continue;
+                }
+                else if (n % actualNumber == 0)
+                {
+                    divisors++;
+                }
+            }
+
+            return divisors;
+
+        }
+    }
+}
