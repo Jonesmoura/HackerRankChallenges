dcd4ee821c8e69ae682fbb021f5ec49c6cfc8159
Jones Moura <jones-ms@live.com>
2024-01-03 15:55:39 -0300

    ExtraLongFactorials Challenge

---
+++ ExtraLongFactorials.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace getTotalX
+{
+    public class ExtraLongFactorials
+    {
+        public static void extraLongFactorials(int n)
+        {
+            BigInteger result = n;
+
+            while (n != 1)
+            {
+                n--;
+                result *= n;
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
