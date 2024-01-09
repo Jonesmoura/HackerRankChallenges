dc869178dcd9447ae7dfcaeddcbeab4bd558f3e5
Jones Moura <jones-ms@live.com>
2024-01-09 20:10:49 -0300

    Sherlock and Squares Challenge

---
+++ Squares.cs
@@ -0,0 +1,12 @@
+﻿namespace getTotalX
+{
+    public class Squares
+    {
+        public static int squares(int a, int b)
+        {
+            int ceil = (int)Math.Floor(Math.Sqrt(b));
+            int floor = (int)Math.Ceiling(Math.Sqrt(a));
+            return ceil - floor + 1;
+        }
+    }
+}
